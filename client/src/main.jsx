478561db5f5a48c36f370567b14b69e0a0ea8ecb
abdisamad <gcigale8@gmail.com>
2026-09-09 15:09:59 +0300

    Add GitHub Pages deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
+const Router = import.meta.env.BASE_URL === '/my-portfolio/' ? HashRouter : BrowserRouter;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><Router><App /></Router></React.StrictMode>);
